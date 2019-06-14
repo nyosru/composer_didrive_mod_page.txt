e2f260a8f98c911e98336daed7c5ee206e37ac44
Сергей <nyos@rambler.ru>
2019-06-14 20:11:54 +0500

    no message

--- 1/didrive/index.php
+++ 1/didrive/index.php
@@ -1,138 +1,46 @@
 <?php
 
-
-
-
-
-
-//        $s = $db->prepare('SELECT sql FROM `sqlite_master` WHERE `name` = :table LIMIT 1 ');
-//        $s->execute( array( ':table' => $table ) );
-//        $r = $s->fetchAll();
-//        \f\pa($r);
-
-
-
-
-
-//
-//// echo '<br/>'.__FILE__.' ('.__LINE__.')';
-//
-///**
-// * добавление записи
-// */
-//if (isset($_POST['addnew']{1})) {
-//
-//    try {
-//
-//        Nyos\mod\items::addNew( $db, $vv['folder'], $vv['now_level'], $_POST, $_FILES );
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . 'Запись добавлена';
-//
-//    } catch (Exception $e) {
-//
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . 'Произошла неописуемая ситуация #' . $e->getCode() . '.' . $e->getLine() . ' (ошибка: ' . $e->getMessage() . ' )';
-//
-//    }
-//    
-//}
-////
-//elseif ( 1 == 2 && isset($_REQUEST['addnew']{1})) {
-//
-//    // $_SESSION['status1'] = true;
-//    // $status = '';
-//    // echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    $r = Nyos\mod\items::addNew($db, $vv['folder'], $vv['now_mod'], array('head' => $_REQUEST['head']));
-//    //echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    // f\pa($r);
-//
-//    if (isset($r['status']) && $r['status'] == 'ok') {
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . $r['html'];
-//    }
-//
-//    // echo $status;
-//}
-////
-//elseif (isset($_REQUEST['delete_item_head']{1})) {
-//
-//    // $_SESSION['status1'] = true;
-//    // $status = '';
-//    // echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    // $r = Nyos\mod\items::saveEdit($db, $id_item, $folder, $cfg_mod, $data);
-//    // addNew( $db, $vv['folder'], $vv['now_mod'], array( 'head' => $_REQUEST['head'] ) );
-//    //echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    // f\pa($r);
-//
-//    $db->sql_Query('UPDATE mitems SET `status` = \'delete\' 
-//        WHERE 
-//        `head` = \'' . addslashes($_REQUEST['head']) . '\' 
-//        AND `module` = \'' . addslashes($vv['level']) . '\' 
-//        AND `folder` = \'' . addslashes($vv['folder']) . '\' 
-//        ;');
-//
-//    // echo $status;
-//
-//    Nyos\mod\items::clearCash($vv['folder']);
-//
-//    if (isset($r['status']) && $r['status'] == 'ok') {
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . $r['html'];
-//    }
-//
-//    // echo $status;
-//}
-////
-//elseif (isset($_POST['delete_item_id']{1})) {
-//
-//    // $_SESSION['status1'] = true;
-//    // $status = '';
-//    // echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    // $r = Nyos\mod\items::saveEdit($db, $id_item, $folder, $cfg_mod, $data);
-//    // addNew( $db, $vv['folder'], $vv['now_mod'], array( 'head' => $_REQUEST['head'] ) );
-//    //echo '<br/>'.__FILE__.'['.__LINE__.']';
-//    // f\pa($r);
-//
-//    $db->sql_Query('UPDATE mitems SET `status` = \'delete2\' 
-//        WHERE 
-//        `id` = \'' . addslashes($_POST['id']) . '\' 
-//        AND `module` = \'' . addslashes($vv['level']) . '\' 
-//        AND `folder` = \'' . addslashes($vv['folder']) . '\' 
-//        ;');
-//
-//    // echo $status;
-//
-//    Nyos\mod\items::clearCash($vv['folder']);
-//
-//    if (isset($r['status']) && $r['status'] == 'ok') {
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . $r['html'];
-//    }
-//
-//    // echo $status;
-//}
-///**
-// * сохранение редактирования
-// */ 
-//elseif (isset($_REQUEST['save_id']) && is_numeric($_REQUEST['save_id']) && isset($_REQUEST['save_edit'])) {
-//
-//    $d = $_POST;
-//    unset($d['addnew']);
-//    $d['files'] = $_FILES;
-//
-//    $r = Nyos\mod\items::saveEdit($db, $_REQUEST['save_id'], $vv['folder'], $vv['now_mod'], $d);
-//    if (isset($r['status']) && $r['status'] == 'ok') {
-//        $vv['warn'] .= ( isset($vv['warn']{3}) ? '<br/>' : '' ) . $r['html'];
-//    }
-//}
-//elseif( isset($_GET['refresh_cash']) && $_GET['refresh_cash'] == 'da' ){
-//    \Nyos\mod\items::clearCash($vv['folder']);
-//}
-//
-//
-
-
 $vv['krohi'] = [];
 $vv['krohi'][1] = array(
     'text' => $vv['now_level']['name'],
     'uri' => $vv['now_level']['cfg.level']
 );
-//$vv['list'] = \Nyos\mod\items::getItems( $db, $vv['folder'], $vv['now_level']['cfg.level'], null);
-//\f\pa($vv['list']);
 
-$vv['tpl_body'] = \f\like_tpl('body', dir_mods_mod_vers_didrive_tpl,  dir_site_module_nowlev_tpldidr, DR );
+if (isset($_POST['editor'])) {
+    $vv['warn'] .= '<div class="warn" >Данные записаны (<a href="/' . $vv['level'] . '/" target="_blank" >страница на сайте</a>)</div>';
+    file_put_contents(DR . dir_site_module_nowlev_tpl . 'body.page.htm', $_POST['editor']);
+}
+
+$vv['in_body_end'][] = '<script type="text/javascript" charset="utf-8" src="/js/ckeditor.4.5.11/ckeditor.js"></script>';
+$vv['in_body_end'][] = '<script  type="text/javascript" charset="utf-8"  >'
+        . ' CKEDITOR.replace("editor1", { '
+        . ' height: 400, '
+        . ' toolbar: [ '
+        . ' { name: "document", groups: ["mode", "document", "doctools"], items: [\'Source\', \'Maximize\', \'ShowBlocks\', \'Templates\'] }, '
+        . ' { name: "clipboard", groups: ["clipboard", "undo"], items: [\'Cut\', \'Copy\', \'Paste\', \'PasteText\', \'PasteFromWord\', \'-\', \'Undo\', \'Redo\'] }, '
+        . ' { name: "clipboard", groups: [ "clipboard", "undo" ], items: [ "Cut", "Copy", "PasteText", "-", "Undo", "Redo" ] }, '
+        . ' { name: \'editing\', groups: [\'find\', \'selection\', \'spellchecker\'], items: [\'Find\', \'Replace\', \'-\', \'SelectAll\', \'-\', \'Scayt\'] }, '
+        . ' { name: "colors", items: [ "TextColor", "BGColor" ] }, '
+        . ' { name: "basicstyles", groups: [ "basicstyles", "cleanup" ], items: [ "Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript", "-", "RemoveFormat" ] }, '
+        . ' { name: "basicstyles", groups: [ "basicstyles", "cleanup" ], items: [ "Bold", "Italic", "Underline", "-", "RemoveFormat" ] }, '
+        . ' { name: "paragraph", groups: [ "list", "indent", "blocks", "align", "bidi" ], items: [ "NumberedList", "BulletedList", "-", "Outdent", "Indent", "-", "Blockquote", "CreateDiv", "-", "JustifyLeft", "JustifyCenter", "JustifyRight", "JustifyBlock" ] }, '
+        . ' { name: "paragraph", groups: [ "list", "indent", "align" ], items: [ "NumberedList", "BulletedList" ] }, '
+        . ' { name: "links", items: [ "Link", "Unlink" ] }, '
+        . ' { name: "insert", items: [ "Image", "Flash", "Table", "HorizontalRule", "Smiley", "SpecialChar", "PageBreak", "Iframe" ] }, '
+        . ' { name: "styles", items: [ "Styles", "Format", "Font", "FontSize" ] } '
+        . ' ] } ); </script>';
+
+// mini
+/*
+  $vv['in_body_end'][] = '<script  type="text/javascript" charset="utf-8"  >'
+  . ' CKEDITOR.replace(\'' . addslashes($k) . '\', { toolbar: [ '
+  . ' { name: "clipboard", groups: [ "clipboard", "undo" ], items: [ "Cut", "Copy", "PasteText", "-", "Undo", "Redo" ] }, '
+  . ' { name: "colors", items: [ "TextColor", "BGColor" ] }, '
+  . ' { name: "basicstyles", groups: [ "basicstyles", "cleanup" ], items: [ "Bold", "Italic", "Underline", "-", "RemoveFormat" ] }, '
+  . ' { name: "paragraph", groups: [ "list", "indent", "align" ], items: [ "NumberedList", "BulletedList" ] } '
+  . ' ] } ); </script>';
+ */
+
+$vv['html'] = file_exists(DR . dir_site_module_nowlev_tpl . 'body.page.htm') ? file_get_contents(DR . dir_site_module_nowlev_tpl . 'body.page.htm') : '';
+
+$vv['tpl_body'] = \f\like_tpl('body', dir_mods_mod_vers_didrive_tpl, dir_site_module_nowlev_tpldidr, DR);
---
+++ 1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * создаём файлы папки если чего то нет
+ */
+if (!is_dir(DR . dir_site_module_nowlev_tpl)) {
+    mkdir(DR . dir_site_module_nowlev_tpl, 0755);
+    $vv['warn'] .= '<br>создана папка для шаблонных файлов';
+}
+$page = 'body.base.htm';
+if (!file_exists(DR . dir_site_module_nowlev_tpl . $page)) {
+    $vv['warn'] .= '<br>создан новый файл ' . $page;
+    file_put_contents(DR . dir_site_module_nowlev_tpl . $page, ' {% include dir_site_module_nowlev_tpl~\'body.page.htm\' %} ');
+}
+$page = 'body.page.htm';
+if (!file_exists(DR . dir_site_module_nowlev_tpl . $page)) {
+    $vv['warn'] .= '<br>создан новый файл ' . $page;
+    file_put_contents(DR . dir_site_module_nowlev_tpl . $page, ' <p>новый файл + отредактируйте файл в панели управления</p> ');
+}
+
+$vv['tpl_body'] = dir_site_module_nowlev_tpl . 'body.base.htm';
