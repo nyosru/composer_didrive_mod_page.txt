4c42de488584a0e28938299e652711d5ae79faa9
Сергей <nyos@rambler.ru>
2020-01-21 11:44:30 +0500

    1.0.1

--- 1/index.php
+++ 1/index.php
@@ -8,14 +8,19 @@ if (!is_dir(DR . dir_site_module_nowlev_tpl)) {
     $vv['warn'] .= '<br>создана папка для шаблонных файлов';
 }
 $page = 'body.base.htm';
+
 if (!file_exists(DR . dir_site_module_nowlev_tpl . $page)) {
     $vv['warn'] .= '<br>создан новый файл ' . $page;
     file_put_contents(DR . dir_site_module_nowlev_tpl . $page, ' {% include dir_site_module_nowlev_tpl~\'body.page.htm\' %} ');
 }
+
 $page = 'body.page.htm';
+
 if (!file_exists(DR . dir_site_module_nowlev_tpl . $page)) {
     $vv['warn'] .= '<br>создан новый файл ' . $page;
     file_put_contents(DR . dir_site_module_nowlev_tpl . $page, ' <p>новый файл + отредактируйте файл в панели управления</p> ');
 }
+
+$vv['tpl_body_in'] = dir_site_module_nowlev_tpl.'body.page.htm';
 
 $vv['tpl_body'] = dir_site_module_nowlev_tpl . 'body.base.htm';
